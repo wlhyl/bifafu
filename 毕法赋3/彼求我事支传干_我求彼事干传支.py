0f13fddee4f54bad2a8c13769ec38900ea317cf6
penguin <penguin@DESKTOP-64O9E02>
2017-04-13 22:24:15 +0800

    金日逢丁

---
+++ 毕法赋3/彼求我事支传干_我求彼事干传支.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from prettytable import PrettyTable
+from ganzhiwuxin import *
+from shipan import *
+
+# 癸酉日第五局
+print(栻盘(天盘(支(8), 支(12)), 干支(干(10), 支(8))))
+print()
+
+# 丁亥日第十一局
+print(栻盘(天盘(支(12), 支(10)), 干支(干(4), 支(10))))
+print()
+
+# 庚戌日，四绝体盘
+print(栻盘(天盘(支(11), 支(4)), 干支(干(7), 支(9))))
+print()
+
+# 辛未日、四墓覆生盘
+print(栻盘(天盘(支(6), 支(1)), 干支(干(8), 支(6))))
+print()
---
+++ 毕法赋3/金日逢丁凶祸动.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from prettytable import PrettyTable
+from ganzhiwuxin import *
+from shipan import *
+
+# 辛巳日、辰时子将
+print(栻盘(天盘(支(11), 支(3)), 干支(干(8), 支(4))))
+print()
+
+# 辛酉日、第五局
+print(栻盘(天盘(支(5), 支(9)), 干支(干(8), 支(8))))
+print()
+
+# 乙巳日、第十一局
+print(栻盘(天盘(支(5), 支(3)), 干支(干(2), 支(4))))
+print()
+
+# 庚辰日、第四局
+print(栻盘(天盘(支(4), 支(7)), 干支(干(7), 支(3))))
+print()
